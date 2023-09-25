9b5543460f73c490054afcd83011cf7d3cf41a86
EnderTheOne <alopez2004@protonmail.com>
2023-09-24 23:57:15 -0400

    Continued development on improved CharacterCreate1

--- CharacterCreate1.java
+++ CharacterCreate1.java
@@ -17,28 +17,37 @@ public class CharacterCreate1
 {
     private int skillPoints = 100;
 
-    int input;
+    private String name = null;
+    private double health;
+    private int attack;
+    private int defense;
+    private int speed;
+    private double luck;
+    
+    private boolean name_Check = false;
+    private boolean health_Check = false;
+    private boolean attack_Check = false;
+    private boolean defense_Check = false;
+    private boolean speed_Check = false;
+    private boolean luck_Check = false;
+    
+    private final int stat_Max = 99;
+    private final int stat_Min = 0;
 
-    String name = "";
-    double health = 0;
-    int attack = 0;
-    int defense = 0;
-    int speed = 0;
-    double luck = 0;
-
-    Scanner keyboard;
+    private Scanner keyboard;
 
     /**
      * Constructor for objects of class CharacterCreate1 
      */
     public CharacterCreate1()
     {
-        staggeredPrint("         V\n __  W   |\n|__|[\"] /.\n|__|/ \\/\n   /___\\ \n   _| |_");
-        
-        //keyboard = new Scanner(System.in);
-        
-        //setName();
-        //setHealth();
+        keyboard = new Scanner(System.in);
+
+        setName();
+        setHealth();
+        setAttack();
+        setDefense();
+        setSpeed();
     }
 
     /**
@@ -59,7 +68,7 @@ public class CharacterCreate1
      */
     public void setName(){
         System.out.println("What is your name?");
-        while(name.isEmpty()){
+        while(name == null || name.trim().isEmpty()){
             name = keyboard.nextLine().trim();
             if(name.isEmpty()){
                 System.out.println("Please enter a valid name.");
@@ -74,38 +83,116 @@ public class CharacterCreate1
         printAvailableSkillPoints();
 
         System.out.println("How much health would you like?");
-        while(health == 0){
+        while(!health_Check || health < 0 || health > 99){
             try{
                 health = keyboard.nextInt();
 
                 if(health <= 0){
-                    System.out.println("\nYou cannot set your health to or below 0.");
+                    System.out.println("\nYou cannot set your health below 0.");
                 }else if(health > 99){
                     System.out.println("\nYou cannot set your health above 99.");
-                    health = 0;
+                }else{
+                    skillPoints -= health;
+                    health_Check = true;
                 }
             }catch(Exception e){
-                System.out.println("\nPlease input a valid integer input.");
-                keyboard.next();
+                System.out.println("\nPlease input a valid integer.");
+                keyboard.nextLine();
             }
         }
         System.out.println("\nHealth: " + health);
     }
-    
+
     /**
      * Method that lets a user set their attack
      */
     public void setAttack(){
         printAvailableSkillPoints();
-        
+
         System.out.println("How much attack would you like?");
+        while(!attack_Check || attack < 0 || attack > 99){
+            try{
+                attack = keyboard.nextInt();
+
+                if(attack < 0){
+                    System.out.println("\nYou cannot set your attack below zero.");
+                }else if(attack  > 99){
+                    System.out.println("\nYou cannot set your attack above 99.");
+                }else{
+                    skillPoints -= attack;
+                    attack_Check = true;
+                }
+            }catch(Exception e){
+                System.out.println("\nPlease input a valid integer.");
+                keyboard.nextLine();
+            }
+        }
+        System.out.println("\nAttack: " + attack);
+    }
+    
+    /**
+     * Method that lets a user set their defense
+     */
+    public void setDefense(){
+        printAvailableSkillPoints();
+
+        System.out.println("How much defense would you like?");
+        while(!defense_Check || defense < 0 || defense > 99){
+            try{
+                defense = keyboard.nextInt();
+
+                if(defense < 0){
+                    System.out.println("\nYou cannot set your defense below zero.");
+                }else if(defense  > 99){
+                    System.out.println("\nYou cannot set your defense above 99.");
+                }else{
+                    skillPoints -= defense;
+                    defense_Check = true;
+                }
+            }catch(Exception e){
+                System.out.println("\nPlease input a valid integer.");
+                keyboard.nextLine();
+            }
+        }
+        System.out.println("\nDefense: " + defense);
+    }
+    
+    /**
+     * Method that lets a user set their speed value
+     */
+    public void setSpeed(){
+        printAvailableSkillPoints();
+
+        System.out.println("How much speed would you like?");
+        while(!speed_Check || speed < 0 || speed > 99){
+            try{
+                speed = keyboard.nextInt();
+
+                if(speed < 0){
+                    System.out.println("\nYou cannot set your speed below zero.");
+                }else if(speed  > 99){
+                    System.out.println("\nYou cannot set your speed above 99.");
+                }else{
+                    skillPoints -= speed;
+                    speed_Check = true;
+                }
+            }catch(Exception e){
+                System.out.println("\nPlease input a valid integer input.");
+                keyboard.nextLine();
+            }
+        }
+        System.out.println("\nSpeed: " + speed);
     }
 
     /**
      * Method that prints available skill points
      */
     public void printAvailableSkillPoints(){
-        System.out.println("\n" + name + ", you have " + skillPoints + " available skill points.");
+        if(skillPoints <= 0){
+            System.out.println("You have no more skill points left.");
+        }else{
+            System.out.println("\n" + name + ", you have " + skillPoints + " available skill points.");
+        }
     }
 
     /**
@@ -125,7 +212,6 @@ public class CharacterCreate1
         System.out.print("\n");
     }
 
-    
     //get methods
     public String getName(){
         return name;
